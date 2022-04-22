ddcde854d9cd0acb7c80fa143a47e04cc7c85276
PavloShutz <99386195+PavloShutz@users.noreply.github.com>
2022-04-22 18:06:34 +0300

    Update own_generator.py

--- own_generator.py
+++ own_generator.py
@@ -1,5 +1,19 @@
 # Own generator. Please, don't punish me :(
-def generator(end, start=0, step=1):
-    while start < end:
-        yield start
-        start += step
+def generator(start=0, end=0, step=1):
+    if start > end:
+        if step < 0:
+            while start > end:
+                yield start
+                start += step
+    if start == 0 and end != 0:
+        while start < end:
+            yield start
+            start += step
+    elif start != 0 and end == 0:
+        while end < start:
+            yield end
+            end += step
+    elif start != 0 and end != 0:
+        while start < end:
+            yield start
+            start += step
