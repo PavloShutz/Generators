96f3a68bfcee9a3c9528f2f6668a6e6482a23612
PavloShutz <99386195+PavloShutz@users.noreply.github.com>
2022-04-23 15:11:49 +0300

    Update own_generator.py

--- own_generator.py
+++ own_generator.py
@@ -1,14 +1,18 @@
 # Own generator. Please, don't punish me :(
 def generator(start=0, end=0, step=1):
+    if step == 0:
+        raise ValueError("generator() arg 3 must not be zero")
     if start > end:
         if step < 0:
             while start > end:
                 yield start
                 start += step
     if start == 0 and end != 0:
-        while start < end:
-            yield start
-            start += step
+        if end > 0:
+            if step > 0:
+                while start < end:
+                    yield start
+                    start += step
     elif start != 0 and end == 0:
         while end < start:
             yield end
