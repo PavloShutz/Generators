9b266393f008365a0e56795596d3917437d8d62c
PavloShutz <99386195+PavloShutz@users.noreply.github.com>
2022-04-26 12:01:11 +0300

    Update own_generator.py

--- own_generator.py
+++ own_generator.py
@@ -27,3 +27,20 @@ def generator(start=0, end=0, step=1):
                 while start < end:
                     yield start
                     start += step
+
+# this is refactored code) 
+def own_range(start=0, end=0, step=1):
+	if step == 0:
+		raise ValueError("own_range() arg 3 must be not zero")
+	if start > end and step < 0:
+		while start > end:
+			yield start 
+			start += step
+	elif start > end or (start != 0 or end == 0) and start != 0 and end == 0:
+		while end < start:
+			yield end
+			end += step
+	elif start == 0 and end != 0 and end > 0 and step > 0 or (start != 0 or end == 0) and start != 0 and start < end and step > 0:
+		while start < end:
+			yield start
+			start += step
